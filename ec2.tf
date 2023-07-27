86c67ba9602d5614c9e519c404cf13260a9db72a
nrasakatla <r.naveenkumar37@gmail.com>
2023-07-27 05:54:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
  #    count = 2
  #    count = var.environment == "dev" || var.environment == "DEV" ? 2 : 1
  #    ami = lookup(var.imagename, var.aws_region)
-      ami = "ami-0d857ff0f5fc4e03b"
+      ami = "ami-05548f9cecf47b442"
      #ami = "${data.aws_ami.my_ami.id}"
      availability_zone = "us-east-1a"
      instance_type = "t2.micro"
